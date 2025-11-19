869056bb61fe7182a2a3d17d2d54b9f03f85eb84
constantinbender51-cmyk <constantinbender51@gmail.com>
2025-11-19 16:37:27 +0100

    AI: write reverse_joke.py line 1

--- reverse_joke.py
+++ reverse_joke.py
@@ -3,6 +3,17 @@ def reverse_words(text):
     reversed_words = words[::-1]
     return ' '.join(reversed_words)
 
-joke = "Why did the chicken cross the road? To get to the other side!"
-reversed_joke = reverse_words(joke)
-print(reversed_joke)
+joke1 = "Why did the chicken cross the road? To get to the other side!"
+joke2 = "What do you call a fake noodle? An impasta!"
+joke3 = "Why don't scientists trust atoms? Because they make up everything!"
+
+reversed_joke1 = reverse_words(joke1)
+reversed_joke2 = reverse_words(joke2)
+reversed_joke3 = reverse_words(joke3)
+
+print("First joke reversed:")
+print(reversed_joke1)
+print("\nSecond joke reversed:")
+print(reversed_joke2)
+print("\nThird joke reversed:")
+print(reversed_joke3)
